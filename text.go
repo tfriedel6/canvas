8a66ff2166d91be38ea298eb0b3a3d8f1bffbc60
Thomas Friedel <tomee6@gmail.com>
2018-01-29 13:14:42 +0100

    moved a freetype source file into the project and optimized it for the given purpose, only upload rendered text rectangle to the texture

--- text.go
+++ text.go
@@ -11,7 +11,7 @@ import (
 	"golang.org/x/image/math/fixed"
 )
 
-var fontRenderingContext = freetype.NewContext()
+var fontRenderingContext = newFRContext()
 
 type Font struct {
 	font *truetype.Font
@@ -56,30 +56,42 @@ func (cv *Canvas) FillText(str string, x, y float32) {
 		gli.TexParameteri(gl_TEXTURE_2D, gl_TEXTURE_MAG_FILTER, gl_NEAREST)
 		gli.TexParameteri(gl_TEXTURE_2D, gl_TEXTURE_WRAP_S, gl_CLAMP_TO_EDGE)
 		gli.TexParameteri(gl_TEXTURE_2D, gl_TEXTURE_WRAP_T, gl_CLAMP_TO_EDGE)
+		gli.TexImage2D(gl_TEXTURE_2D, 0, gl_RGBA, int32(cv.w), int32(cv.h), 0, gl_RGBA, gl_UNSIGNED_BYTE, nil)
 	}
 
-	for i := range cv.text.target.Pix {
-		cv.text.target.Pix[i] = 0
-	}
-
-	fontRenderingContext.SetFont(cv.text.font.font)
-	fontRenderingContext.SetFontSize(float64(cv.text.size))
-	fontRenderingContext.SetSrc(image.NewUniform(colorGLToGo(cv.fill.r, cv.fill.g, cv.fill.b, cv.fill.a)))
-	fontRenderingContext.SetDst(cv.text.target)
-	fontRenderingContext.SetClip(cv.text.target.Bounds())
-	fontRenderingContext.DrawString(str, fixed.Point26_6{X: fixed.Int26_6(x*64 + 0.5), Y: fixed.Int26_6(y*64 + 0.5)})
+	fontRenderingContext.setFont(cv.text.font.font)
+	fontRenderingContext.setFontSize(float64(cv.text.size))
+	fontRenderingContext.setSrc(image.NewUniform(colorGLToGo(cv.fill.r, cv.fill.g, cv.fill.b, cv.fill.a)))
+	fontRenderingContext.setDst(cv.text.target)
+	fontRenderingContext.setClip(cv.text.target.Bounds())
+	_, bounds, _ := fontRenderingContext.drawString(str, fixed.Point26_6{X: fixed.Int26_6(x*64 + 0.5), Y: fixed.Int26_6(y*64 + 0.5)})
+	subImg := cv.text.target.SubImage(bounds).(*image.RGBA)
 
 	gli.BlendFunc(gl_ONE, gl_ONE_MINUS_SRC_ALPHA)
 
 	gli.ActiveTexture(gl_TEXTURE0)
 	gli.BindTexture(gl_TEXTURE_2D, cv.text.tex)
-	gli.TexImage2D(gl_TEXTURE_2D, 0, gl_RGBA, int32(cv.w), int32(cv.h), 0, gl_RGBA, gl_UNSIGNED_BYTE, gli.Ptr(&cv.text.target.Pix[0]))
+
+	for y, w, h := 0, bounds.Dx(), bounds.Dy(); y < h; y++ {
+		off := y * subImg.Stride
+		pix := subImg.Pix
+		gli.TexSubImage2D(gl_TEXTURE_2D, 0, 0, int32(cv.h-1-y), int32(w), 1, gl_RGBA, gl_UNSIGNED_BYTE, gli.Ptr(&pix[off]))
+		for b := w * 4; b > 0; b-- {
+			pix[off] = 0
+			off++
+		}
+	}
 
 	gli.UseProgram(tr.id)
 	gli.Uniform1i(tr.image, 0)
 
 	gli.BindBuffer(gl_ARRAY_BUFFER, buf)
-	data := [16]float32{-1, -1, -1, 1, 1, 1, 1, -1, 0, 1, 0, 0, 1, 0, 1, 1}
+	x0 := float32(bounds.Min.X) / cv.fw
+	y0 := float32(bounds.Min.Y) / cv.fh
+	x1 := float32(bounds.Max.X) / cv.fw
+	y1 := float32(bounds.Max.Y) / cv.fh
+	data := [16]float32{x0*2 - 1, -y0*2 + 1, x0*2 - 1, -y1*2 + 1, x1*2 - 1, -y1*2 + 1, x1*2 - 1, -y0*2 + 1,
+		0, 1, 0, 1 - (y1 - y0), x1 - x0, 1 - (y1 - y0), x1 - x0, 1}
 	gli.BufferData(gl_ARRAY_BUFFER, len(data)*4, unsafe.Pointer(&data[0]), gl_STREAM_DRAW)
 
 	gli.VertexAttribPointer(tr.vertex, 2, gl_FLOAT, false, 0, nil)
