1703c1a8fdc22794c4435c79cfecbf4c29af25e3
Thomas Friedel <tomee6@gmail.com>
2018-03-28 11:29:58 +0200

    added documentation; added input event functions

--- sdlcanvas/sdlcanvas.go
+++ sdlcanvas/sdlcanvas.go
@@ -14,6 +14,8 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+// Window represents the opened window with GL context. The Mouse* and Key*
+// functions can be set for callbacks
 type Window struct {
 	Window     *sdl.Window
 	GLContext  sdl.GLContext
@@ -22,9 +24,17 @@ type Window struct {
 	frameCount int
 	fps        float32
 	close      bool
+	events     []sdl.Event
+	Event      func(event sdl.Event)
+	MouseDown  func(button, x, y int)
+	MouseMove  func(x, y int)
+	MouseUp    func(button, x, y int)
+	KeyDown    func(scancode int, rune rune, name string)
+	KeyUp      func(scancode int, rune rune, name string)
 }
 
-func CreateCanvasWindow(w, h int, title string) (*Window, *canvas.Canvas, error) {
+// CreateWindow creates a window using SDL and initializes the OpenGL context
+func CreateWindow(w, h int, title string) (*Window, *canvas.Canvas, error) {
 	runtime.LockOSThread()
 
 	// init SDL
@@ -73,54 +83,87 @@ func CreateCanvasWindow(w, h int, title string) (*Window, *canvas.Canvas, error)
 	wnd := &Window{
 		Window:    window,
 		GLContext: glContext,
+		events:    make([]sdl.Event, 0, 100),
 	}
 
 	return wnd, cv, nil
 }
 
+// Destroy destroys the GL context and the window
 func (wnd *Window) Destroy() {
 	sdl.GL_DeleteContext(wnd.GLContext)
 	wnd.Window.Destroy()
 }
 
+// FPS returns the frames per second (averaged over 10 frames)
 func (wnd *Window) FPS() float32 {
 	return wnd.fps
 }
 
+// Close can be used to end a call to MainLoop
 func (wnd *Window) Close() {
 	wnd.close = true
 }
 
-func (wnd *Window) IsClosed() bool {
-	return wnd.close
-}
-
+// StartFrame handles events and gets the window ready for rendering
 func (wnd *Window) StartFrame() error {
-	for {
-		ei := sdl.PollEvent()
-		if ei == nil {
-			break
-		}
-		switch e := ei.(type) {
-		case *sdl.WindowEvent:
-			if e.Event == sdl.WINDOWEVENT_CLOSE {
-				wnd.close = true
-			}
-		case *sdl.KeyDownEvent:
-			if e.Keysym.Scancode == sdl.SCANCODE_ESCAPE {
-				wnd.close = true
-			}
-		}
-	}
-
 	err := sdl.GL_MakeCurrent(wnd.Window, wnd.GLContext)
 	if err != nil {
 		return err
 	}
 
+	wnd.events = wnd.events[:0]
+	for {
+		event := sdl.PollEvent()
+		if event == nil {
+			break
+		}
+
+		handled := false
+		switch e := event.(type) {
+		case *sdl.MouseButtonEvent:
+			if e.Type == sdl.MOUSEBUTTONDOWN {
+				if wnd.MouseDown != nil {
+					wnd.MouseDown(int(e.Button), int(e.X), int(e.Y))
+					handled = true
+				}
+			} else if e.Type == sdl.MOUSEBUTTONUP {
+				if wnd.MouseUp != nil {
+					wnd.MouseUp(int(e.Button), int(e.X), int(e.Y))
+					handled = true
+				}
+			}
+		case *sdl.MouseMotionEvent:
+			if wnd.MouseMove != nil {
+				wnd.MouseMove(int(e.X), int(e.Y))
+				handled = true
+			}
+		case *sdl.KeyDownEvent:
+			if wnd.KeyDown != nil {
+				wnd.KeyDown(int(e.Keysym.Scancode), rune(e.Keysym.Unicode), keyName(e.Keysym.Scancode))
+				handled = true
+			}
+		case *sdl.KeyUpEvent:
+			if wnd.KeyUp != nil {
+				wnd.KeyUp(int(e.Keysym.Scancode), rune(e.Keysym.Unicode), keyName(e.Keysym.Scancode))
+				handled = true
+			}
+		}
+
+		if !handled && wnd.Event != nil {
+			wnd.Event(event)
+			handled = true
+		}
+
+		if !handled {
+			wnd.events = append(wnd.events, event)
+		}
+	}
+
 	return nil
 }
 
+// FinishFrame updates the FPS count and displays the frame
 func (wnd *Window) FinishFrame() {
 	now := time.Now()
 	wnd.frameTimes[wnd.frameIndex] = now
@@ -136,7 +179,8 @@ func (wnd *Window) FinishFrame() {
 	sdl.GL_SwapWindow(wnd.Window)
 }
 
-func (wnd *Window) MainLoop(drawFunc func()) {
+// MainLoop runs a main loop and calls run on every frame
+func (wnd *Window) MainLoop(run func()) {
 	// main loop
 	for !wnd.close {
 		err := wnd.StartFrame()
@@ -145,7 +189,20 @@ func (wnd *Window) MainLoop(drawFunc func()) {
 			continue
 		}
 
-		drawFunc()
+		for _, event := range wnd.events {
+			switch e := event.(type) {
+			case *sdl.WindowEvent:
+				if e.Event == sdl.WINDOWEVENT_CLOSE {
+					wnd.close = true
+				}
+			case *sdl.KeyDownEvent:
+				if e.Keysym.Scancode == sdl.SCANCODE_ESCAPE {
+					wnd.close = true
+				}
+			}
+		}
+
+		run()
 
 		wnd.FinishFrame()
 	}
